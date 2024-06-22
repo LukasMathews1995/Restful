719a79dbee629dd1828982ec0f41c478dae02de8
Lucas Mathews <lucasmathews50@gmail.com>
2024-06-22 00:10:20 -0300

    commit-deucerto

---
+++ santander-dev-week-2023/src/main/java/me/dio/santander_dev_week_2023/domain/model/Card.java
@@ -0,0 +1,50 @@
+package me.dio.santander_dev_week_2023.domain.model;
+
+
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+
+
+
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "limite", scale = 13,precision = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    
+ 
+
+}
---
+++ santander-dev-week-2023/src/main/java/me/dio/santander_dev_week_2023/domain/model/News.java
@@ -0,0 +1,10 @@
+package me.dio.santander_dev_week_2023.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name="tb_news")
+public class News extends BaseItem{
+    
+
+
+}
